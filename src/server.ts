15d9bda0d7ca89345c2655ecbd8f170887216788
Andrew Marcuse <fileformat@gmail.com>
2019-11-23 15:18:33 -0500

    data cleanup, flag for unknown location

--- src/server.ts
+++ src/server.ts
@@ -183,6 +183,7 @@ rootRouter.get('/resolvers/:resolver/index.html', async (ctx: any) => {
     return;
   }
   ctx.body = await ctx.render('resolvers-detail.hbs', {
+    hostname: ctx.query.hostname,
     title: resolverData.name,
     titleimg: resolverData.icon,
     resolver: resolverData
@@ -249,6 +250,52 @@ rootRouter.post('/dns-check.html', async (ctx:any) => {
     stream.write(`</div>`);
 
     stream.write(`<p><a class="btn btn-primary" href="/dns-check.html?hostname=${encodeURIComponent(hostname)}">Continue</a>`);
+  });
+});
+
+rootRouter.post('/resolvers/:resolver/index.html', async (ctx: any) => {
+
+  const resolverData = resolvers.get(ctx.params.resolver);
+  if (!resolverData) {
+    ctx.flash('error', `Sorry, we don't have any info for an open resolver "${ctx.params.resolver}".`)
+    ctx.redirect("/resolvers/index.html");
+    return;
+  }
+
+  const hostname = ctx.request.body.hostname;
+  if (!hostname) {
+    ctx.flash('error', 'You must enter a hostname to check!');
+    ctx.redirect('index.html');
+    return;
+  }
+
+  if (!psl.isValid(hostname)) {
+    ctx.flash('error', `${Handlebars.escapeExpression(hostname)} is not a valid hostname!`);
+    ctx.redirect(`index.html?hostname=${encodeURIComponent(hostname)}`);
+    return;
+  }
+
+  streamer.streamResponse(ctx, `DNS Results from ${resolverData.name} for ${hostname}`, async (stream) => {
+
+    for (const ipv4 of resolverData.ipv4) {
+      const dnsResolver = new dnsPromises.Resolver();
+      dnsResolver.setServers([ipv4]);
+      stream.write(`<p>${ipv4}: `);
+      const results = await dnsResolver.resolve4(hostname);
+      stream.write("<ul>")
+      for (const result of results) {
+        stream.write(`<li>${result} `);
+        stream.write(`(${await reverseDns(dnsResolver, result)})`);
+        stream.write(`</li>`);
+      }
+      stream.write("</ul>")
+    }
+
+    stream.write(`<div class="alert alert-info">`);
+    stream.write(`Complete`);
+    stream.write(`</div>`);
+
+    stream.write(`<p><a class="btn btn-primary" href="index.html?hostname=${encodeURIComponent(hostname)}">Continue</a>`);
   });
 });
 
