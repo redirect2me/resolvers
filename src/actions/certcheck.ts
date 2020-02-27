1f3b5069a707797743841bd9b49ba72ff73079d4
Andrew Marcuse <fileformat@gmail.com>
2020-02-27 18:46:02 -0500

    progress on checks

--- src/actions/certcheck.ts
+++ src/actions/certcheck.ts
@@ -4,6 +4,7 @@ import Router from 'koa-router';
 import * as psl from 'psl';
 import * as http from 'http';
 import * as https from 'https';
+import * as tls from 'tls';
 
 import { logger } from '../logger';
 import * as streamer from '../streamer';
@@ -35,22 +36,32 @@ certCheckRouter.post('/http/cert-check.html', async (ctx:any) => {
 
   streamer.streamResponse(ctx, `Certificate check for ${hostname}`, async (stream) => {
 
+    let expires:string|null = null;
+
     const options = {
         agent: false,
         checkServerIdentity: function(host:string, cert:any) {
             do {
-                stream.write(`<h2>Certificate for ${cert.subject.CN}</h2>`)
+                if (expires == null && cert.valid_to) {
+                    expires = cert.valid_to;
+                }
+                stream.write("<details>");
+                stream.write(`<summary>Certificate for ${cert.subject.CN}</summary>`)
                 stream.write(`<table class="table table-striped"><tbody>`)
-            stream.write(`<tr><td>Subject</td><td><pre>${JSON.stringify(cert.subject, null, 2)}</pre></td></tr>`);
-            stream.write(`<tr><td>Issuer</td><td><pre>${JSON.stringify(cert.issuer, null, 2)}</pre></td></tr>`);
-            stream.write(`<tr><td>Valid from</td><td>${cert.valid_from}</td></tr>`);
-            stream.write(`<tr><td>Valid to</td><td>${cert.valid_to}</td></tr>`);
-            stream.write(`<tr><td>Fingerprint</td><td>${cert.fingerprint256}</td></tr>`);
-            stream.write(`<tr><td>Serial number</td><td>${cert.serialNumber}</td></tr>`);
-            stream.write(`</tbody></table>`);
-            logger.info({ cert }, "cert");
+                stream.write(`<tr><td>Subject</td><td><pre>${JSON.stringify(cert.subject, null, 2)}</pre></td></tr>`);
+                stream.write(`<tr><td>Issuer</td><td><pre>${JSON.stringify(cert.issuer, null, 2)}</pre></td></tr>`);
+                stream.write(`<tr><td>Valid from</td><td>${cert.valid_from}</td></tr>`);
+                stream.write(`<tr><td>Valid to</td><td>${cert.valid_to}</td></tr>`);
+                stream.write(`<tr><td>Fingerprint</td><td>${cert.fingerprint256}</td></tr>`);
+                stream.write(`<tr><td>Serial&nbsp;number</td><td>${cert.serialNumber}</td></tr>`);
+                stream.write(`</tbody></table>`);
+                stream.write("</details>");
+            logger.info({ subject: cert.subject, issuer: cert.issuerCertificate ? cert.issuerCertificate.subject : '(none)'}, "cert");
+            if (cert == cert.issuerCertificate) {
+                break;
+            }
             cert = cert.issuerCertificate;
-            } while (cert && cert != cert.issuerCertificate)
+            } while (cert)
         },
         ciphers: 'ALL',
         hostname,
@@ -64,12 +75,22 @@ certCheckRouter.post('/http/cert-check.html', async (ctx:any) => {
 
     logger.trace( { req, res }, "cert check response");
 
-    stream.write(`<h2>HTTP Response</h2>`);
 
-    //stream.write(`<p>Remote IP=${req.remoteAddress}</p>`)
-    stream.write(`<p>HTTP version=${res.httpVersion}</p>`);
-    stream.write(`<p>Server=${res.headers.server}</p>`)
-    //stream.write(res.)
+    const tlsSocket = res.socket as tls.TLSSocket;
+
+    stream.write("<details>");
+    stream.write(`<summary>HTTP Response</summary>`);
+    stream.write(`<table class="table table-striped"><tbody>`)
+    stream.write(`<tr><td>Authorized</td><td>${tlsSocket.authorized}</td></tr>`);
+    stream.write(`<tr><td>Remote IP</td><td>${res.socket.remoteAddress}</td></tr>`);
+    stream.write(`<tr><td>HTTP version</td><td>${res.httpVersion}</td></tr>`);
+    stream.write(`<tr><td>Server</td><td>${res.headers.server}</td></tr>`);
+    stream.write(`<tr><td>Protocol</td><td>${tlsSocket.getProtocol()}</td></tr>`);
+    stream.write(`<tr><td>Cipher</td><td>${tlsSocket.getCipher().name}}</td></tr>`);
+    stream.write(`</tbody></table>`);
+    stream.write("</details>");
+
+    stream.write(`<p>Expires: ${expires}</p>`);
 
     stream.write(`<div class="alert alert-info">`);
     stream.write(`Complete`);
