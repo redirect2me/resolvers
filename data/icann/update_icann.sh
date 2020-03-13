3f1f1f71738461ffdcb25368212c742ab7fea65f
Andrew Marcuse <fileformat@gmail.com>
2020-03-13 10:12:45 -0400

    only update icann if changed (other than timestamp on first line)

--- data/icann/update_icann.sh
+++ data/icann/update_icann.sh
@@ -2,6 +2,11 @@
 #
 # download the list of TLDs from ICANN
 #
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
 echo "INFO: starting ICANN update at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
 
 TARGET_DIR="$(dirname "$0")"
@@ -9,6 +14,16 @@ echo "INFO: target directory is ${TARGET_DIR}"
 
 URL="https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
 
-curl "${URL}" >${TARGET_DIR}/tlds-alpha-by-domain.txt
+curl --silent "${URL}" >${TARGET_DIR}/tlds-alpha-by-domain.txt
+
+DIFF=$(git diff --name-only "-G^[^#].+" "${TARGET_DIR}/tlds-alpha-by-domain.txt")
+
+if [ "${DIFF}" == "" ]; then
+    echo "INFO: no substantive changes, reverting"
+    # NOTE: this won't work if we aren't within the repo directory, but we generally are
+    git checkout "${TARGET_DIR}/tlds-alpha-by-domain.txt"
+else
+    echo "INFO: actual changes detected"
+fi
 
 echo "INFO: complete ICANN update at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
