411f0b2a9feb445bf05f9565bf308e616b9d29a8
Andrew Marcuse <fileformat@gmail.com>
2020-03-28 13:41:53 -0400

    more 'nice' tlds

--- src/logger.ts
+++ src/logger.ts
@@ -3,35 +3,13 @@ import PinoCaller from 'pino-caller';
 
 import config from './config';
 
-function MovenUserReqSerializer(req: any): any {
-  //console.log("SERIALIZING REQ");
 
-  const result:any = Pino.stdSerializers.req(req);
-
-
-  if (req.user) {
-    //console.log("SERIALIZING REQ USER");
-    result.user = req.user;
-  }
-
-  return result;
-}
-
-const serializers:any = {
-  err: Pino.stdSerializers.err,
-  req: MovenUserReqSerializer,
-  res: Pino.stdSerializers.res,
-};
-
-//serializers[Symbol.for('pino.*')] = MovenAuthContextAndTraceElevator;
-
-const options = {
+const options:Pino.LoggerOptions = {
   // @ts-ignore
   level: config.get('logLevel'),
   name: process.env.npm_package_name || 'moven-newtrx',
   redact: ['err.request', 'err.response', 'req.headers.authorization'],
-  serializers,
-  timestamp: () => `,"time":"${new Date().toISOString()}"`, // So it looks like bunyan
+  timestamp: Pino.stdTimeFunctions.isoTime,
 };
 
 const logger: Pino.Logger = PinoCaller(
